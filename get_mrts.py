c0a3efe3a5bf5a0527cbccfa42e68907a203c55b
jwbensley <1025875+jwbensley@users.noreply.github.com>
2022-01-29 12:53:08 +0100

    Working on get_mrt.py script to pull MRTs regularly

--- get_mrts.py
+++ get_mrts.py
@@ -1,27 +1,128 @@
 import datetime
+import logging
 
 import sys
 sys.path.append('./')
 from mrt_getter import mrt_getter
 from redis_db import redis_db
+from config import config as cfg
 
 def main():
 
-	logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO)
-	logging.info("Starting MRT downloader")
-	g = mrt_getter()
-	rdb = redis_db()
+    logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO)
+    logging.info("Starting MRT downloader")
+    g = mrt_getter()
+    rdb = redis_db()
 
-	#this_month = datetime.datetime.now().strftime("%Y-%m")
-	#in_files = rdb.get_keys(f"IN_FILES")
+    #this_month = datetime.datetime.now().strftime("%Y-%m")
+    #in_files = rdb.get_keys(f"IN_FILES")
 
-	file_list = []
-
-
+    file_list = []
 
 
+    """
+    g.get_range_rv_upd(
+        base_url=cfg.RV_LINX_BASE,
+        dl_dir=cfg.RV_LINX_DIR,
+        end_date="20220101.0200",
+        file_ext=cfg.RV_LINX_FXT,
+        replace=False,
+        start_date="20220101.0000",
+        upd_pfx=cfg.RV_LINX_UPDS,
+    )
+
+    g.get_range_rv_upd(
+        base_url=cfg.RV_SYDNEY_BASE,
+        dl_dir=cfg.RV_SYDNEY_DIR,
+        end_date="20220101.0045",
+        file_ext=cfg.RV_SYDNEY_FXT,
+        replace=False,
+        start_date="20220101.0000",
+        upd_pfx=cfg.RV_SYDNEY_UPDS,
+    )
+    """
+
+    print(
+        g.get_rv_latest_rib(
+            base_url=cfg.RV_LINX_BASE,
+            dl_dir=cfg.RV_LINX_DIR,
+            file_ext=cfg.RV_LINX_FXT,
+            replace=False,
+            rib_pfx=cfg.RV_LINX_RIBS,
+        )
+    )
+
+    print(
+        g.get_rv_latest_upd(
+            base_url=cfg.RV_LINX_BASE,
+            dl_dir=cfg.RV_LINX_DIR,
+            file_ext=cfg.RV_LINX_FXT,
+            replace=False,
+            upd_pfx=cfg.RV_LINX_UPDS,
+        )
+    )
+
+    print(
+        g.get_rv_latest_rib(
+            base_url=cfg.RV_SYDNEY_BASE,
+            dl_dir=cfg.RV_SYDNEY_DIR,
+            file_ext=cfg.RV_SYDNEY_FXT,
+            replace=False,
+            rib_pfx=cfg.RV_SYDNEY_RIBS,
+        )
+    )
+
+    print(
+        g.get_rv_latest_upd(
+            base_url=cfg.RV_SYDNEY_BASE,
+            dl_dir=cfg.RV_SYDNEY_DIR,
+            file_ext=cfg.RV_SYDNEY_FXT,
+            replace=False,
+            upd_pfx=cfg.RV_SYDNEY_UPDS,
+        )
+    )
 
 
+    print(
+        g.get_ripe_latest_rib(
+            base_url=cfg.RCC_23_BASE,
+            dl_dir=cfg.RCC_23_DIR,
+            file_ext=cfg.RCC_23_FXT,
+            replace=False,
+            rib_pfx=cfg.RCC_23_RIBS,
+        )
+    )
+
+    print(
+        g.get_ripe_latest_upd(
+            base_url=cfg.RCC_23_BASE,
+            dl_dir=cfg.RCC_23_DIR,
+            file_ext=cfg.RCC_23_FXT,
+            replace=False,
+            upd_pfx=cfg.RCC_23_UPDS,
+        )
+    )
+
+
+    print(
+        g.get_ripe_latest_rib(
+            base_url=cfg.RCC_24_BASE,
+            dl_dir=cfg.RCC_24_DIR,
+            file_ext=cfg.RCC_24_FXT,
+            replace=False,
+            rib_pfx=cfg.RCC_24_RIBS,
+        )
+    )
+
+    print(
+        g.get_ripe_latest_upd(
+            base_url=cfg.RCC_24_BASE,
+            dl_dir=cfg.RCC_24_DIR,
+            file_ext=cfg.RCC_24_FXT,
+            replace=False,
+            upd_pfx=cfg.RCC_24_UPDS,
+        )
+    )
 
 if __name__ == '__main__':
     main()
--- mrt_getter.py
+++ mrt_getter.py
@@ -1,82 +1,322 @@
 import datetime
+import logging
 import os
 import requests
+
+import sys
+sys.path.append('./')
+from config import config as cfg
+
 
 class mrt_getter:
     """
     Class which can be used to get MRT files from public sources.
     """
 
-    # Locations of popular MRT dump archives:
+    @staticmethod
+    def get_ripe_latest_rib(
+        base_url=None,
+        dl_dir=None,
+        file_ext=None,
+        replace=False,
+        rib_pfx=None,
+    ):
+        """
+        Download the lastest RIB dump MRT from a RIPE MRT archive.
+        RIB dumps are every 8 hours.
+        """
 
-    """
-    Route-Views London
-    RIB dumps are every 2 hours
-    RIB dump example: http://archive.routeviews.org/route-views.linx/bgpdata/2021.12/RIBS/rib.20211222.0600.bz2
-    UPDATE dumps are ever 15 minutes
-    UPDATE dump example: http://archive.routeviews.org/route-views.linx/bgpdata/2021.12/UPDATES/updates.20211222.0600.bz2
-    """
-    RV_LINX = "http://archive.routeviews.org/route-views.linx/bgpdata/"
+        if (not base_url or
+            not file_ext or
+            not rib_pfx or
+            not dl_dir):
 
-    """
-    Route-Views Sydnex
-    RIB dumps are every 2 hours
-    RIB dump example: http://archive.routeviews.org/route-views.sydney/bgpdata/2021.12/RIBS/rib.20211201.0600.bz2
-    UPDATE dumps are ever 15 minutes
-    UPDATE dump example: http://archive.routeviews.org/route-views.sydney/bgpdata/2021.12/UPDATES/updates.20211201.0030.bz2
-    """
-    RV_SYDNEY = "http://archive.routeviews.org/route-views.sydney/bgpdata/"
+            raise ValueError(
+                f"Missing required options {base_url}, {rib_pfx}, {dl_dir}"
+            )
 
+        os.makedirs(dl_dir, exist_ok=True)
+        if not os.path.isdir(dl_dir):
+            raise ValueError(f"Output directory does not exist {dl_dir}")
 
-    """
-    RRC23 Singapore
-    RIB dumps are every 8 hours
-    RIB dump example: https://data.ris.ripe.net/rrc23/2021.12/bview.20211206.1600.gz
-    UPDATE dumps are ever 5 minutes
-    UPDATE dump example: https://data.ris.ripe.net/rrc23/2021.12/updates.20211231.2335.gz
-    """
-    RCC_23 = "https://data.ris.ripe.net/rrc23/"
+        """
+        When downloading rib dumps from RIPE, we calculate the time of the
+        last whole 8 hour interval. RIPE RIB files are dumped every 8 hours,
+        if it's 09.00 we're half way through the 08.00 to 16.00 time period,
+        which will be available from 16.00 onwards. This means that the latest
+        complete RIB dump we can download at 09.00 would be from the 00.00-08.00
+        period and it would be called "0000". h_delta gets us back to 00.00.
 
-    """
-    RRC24 LACNIC
-    RIB dumps are every 8 hours
-    RIB dump example: https://data.ris.ripe.net/rrc24/2021.12/bview.20211208.1600.gz
-    UPDATE dumps are ever 5 minutes
-    UPDATE dump example: https://data.ris.ripe.net/rrc24/2021.12/updates.20211231.1245.gz
-    """
-    RCC_24 = "https://data.ris.ripe.net/rrc24/"
+        If this machine is in a different timezone to the archive server, an
+        additional offset is required, RIPE_RIB_OFFSET.
+        """
+        hours = int(datetime.datetime.strftime(datetime.datetime.now(), "%H"))
+        mod = hours % 8
+        if mod == 0:
+            h_delta = datetime.timedelta(hours=8)
+        else:
+            h_delta = datetime.timedelta(hours=(8 + mod))
+
+        ym = datetime.datetime.strftime(datetime.datetime.now()-h_delta,"%Y.%m")
+        ymd_hm = datetime.datetime.strftime(datetime.datetime.now()-h_delta,"%Y%m%d.%H00")
+
+        rib_url = base_url + ym + rib_pfx + ymd_hm + file_ext
+        filename = dl_dir + os.path.basename(rib_url)
+
+        if (not replace and os.path.exists(filename)):
+                logging.info(f"Skipping existing file {filename}")
+        else:
+            mrt_getter.download_mrt(filename=filename, url=rib_url)
+
+        return (filename, rib_url)
+
 
     @staticmethod
-    def get_latest_rv():
+    def get_ripe_latest_upd(
+        base_url=None,
+        dl_dir=None,
+        file_ext=None,
+        replace=False,
+        upd_pfx=None,
+    ):
 
-        downloaded = []
+        """
+        Download the lastest update MRT file from a RIPE MRT archive.
+        UPDATE dumps are every 5 minutes.
+        """
 
+        if (not base_url or
+            not file_ext or
+            not upd_pfx or
+            not dl_dir):
+
+            raise ValueError(
+                f"Missing required options {base_url}, {upd_pfx}, {dl_dir}"
+            )
+
+        os.makedirs(dl_dir, exist_ok=True)
+        if not os.path.isdir(dl_dir):
+            raise ValueError(f"Output directory does not exist {dl_dir}")
+
+        """
+        When downloading updates from RIPE, we calculate the name of
+        the last complete 5 minute update file. At 09.13 the last complete
+        update file will be called "0905", it will be for the period
+        09.05-09.10. So the filename can be calculatd as:
+        "round down to the last 5 minute whole interval - another 5 minutes".
+
+        If the current time is 09.15 (a round 15 minute interval) a dump
+        should be available called "0910" for 09.10-09.15 period. The archive
+        might be slow to update though.
+
+        To be safe:
+        At 09.13 this function will download the "0905" file (09.05-09.10).
+        At 09.15 this function will download the "0905" file too.
+        At 09.17 this function will download the "0910" file (09.10-09.15).
+
+        If this machine is in a different timezone to the archive server, an
+        additional offset is required, RCC_UPD_OFFSET.
+        """
+        minutes = int(datetime.datetime.strftime(datetime.datetime.now(), "%M"))
+        mod = minutes % 5
+        if mod == 0:
+            m_delta = datetime.timedelta(minutes=10)
+        else:
+            m_delta = datetime.timedelta(minutes=(5 + mod))
+
+        h_delta = datetime.timedelta(hours=cfg.RCC_UPD_OFFSET)
+
+        ym = datetime.datetime.strftime(datetime.datetime.now()-h_delta,"%Y.%m")
+        ymd_hm = datetime.datetime.strftime(datetime.datetime.now()-h_delta-m_delta,"%Y%m%d.%H%M")
+
+        udp_url = base_url + ym + upd_pfx + ymd_hm + file_ext
+        filename = dl_dir + os.path.basename(udp_url)
+
+        if (not replace and os.path.exists(filename)):
+                logging.info(f"Skipping existing file {filename}")
+        else:
+            mrt_getter.download_mrt(filename=filename, url=udp_url)
+
+        return (filename, udp_url)
+
+    @staticmethod
+    def get_rv_latest_rib(
+        base_url=None,
+        dl_dir=None,
+        file_ext=None,
+        replace=False,
+        rib_pfx=None,
+    ):
+        """
+        Download the lastest RIB dump MRT from a route-views MRT archive.
+        RIB dumps are every 2 hours.
+        """
+
+        if (not base_url or
+            not file_ext or
+            not rib_pfx or
+            not dl_dir):
+
+            raise ValueError(
+                f"Missing required options {base_url}, {rib_pfx}, {dl_dir}"
+            )
+
+        os.makedirs(dl_dir, exist_ok=True)
+        if not os.path.isdir(dl_dir):
+            raise ValueError(f"Output directory does not exist {dl_dir}")
+
+        """
+        When downloading rib dumps from route-views, we calculate the
+        time either 2 or 3 hours ago from now(). RV RIB files are dumped
+        every 2 hours, if it's 09.00 we're half way through the 08.00 to 10.00
+        time period, which will be available from 10.00 onwards. This means
+        that the latest complete RIB dump we can download at 09.00 would be
+        from 08.00. h_delta gets us back to 08.00.
+
+        If this machine is in a different timezone to the archive server, an
+        additional offset is required, RV_RIB_OFFSET.
+        """
         hours = int(datetime.datetime.strftime(datetime.datetime.now(), "%H"))
         if hours % 2 != 0:
-            hours = 3
+            hours = 3 + cfg.RV_RIB_OFFSET
         else:
-            hours = 2
+            hours = 2 + cfg.RV_RIB_OFFSET
         h_delta = datetime.timedelta(hours=hours)
 
-        latest_ym = datetime.datetime.strftime(datetime.datetime.now()-h_delta,"%Y.%m")
-        latest_ymd_hm = datetime.datetime.strftime(datetime.datetime.now()-h_delta,"%Y%m%d.%H00")
+        ym = datetime.datetime.strftime(datetime.datetime.now()-h_delta,"%Y.%m")
+        ymd_hm = datetime.datetime.strftime(datetime.datetime.now()-h_delta,"%Y%m%d.%H00")
 
-        latest_linx_rib = mrt_getter.RV_LINX + latest_ym + "/RIBS/rib." + latest_ymd_hm + ".bz2"
-        filename = "/tmp/" + os.path.basename(latest_linx_rib)
-        mrt_getter.download_mrt(filename=filename, url=latest_linx_rib)
-        downloaded.append(latest_linx_rib)
+        rib_url = base_url + ym + rib_pfx + ymd_hm + file_ext
+        filename = dl_dir + os.path.basename(rib_url)
 
-        update_base = datetime.datetime.strptime(latest_ymd_hm, "%Y%m%d.%H00")
-        for i in range(0, 8):
+        if (not replace and os.path.exists(filename)):
+                logging.info(f"Skipping existing file {filename}")
+        else:
+            mrt_getter.download_mrt(filename=filename, url=rib_url)
+
+        return (filename, rib_url)
+
+    @staticmethod
+    def get_rv_latest_upd(
+        base_url=None,
+        dl_dir=None,
+        file_ext=None,
+        replace=False,
+        upd_pfx=None,
+    ):
+
+        """
+        Download the lastest update MRT file from a route-views MRT archive.
+        UPDATE dumps are every 15 minutes.
+        """
+
+        if (not base_url or
+            not file_ext or
+            not upd_pfx or
+            not dl_dir):
+
+            raise ValueError(
+                f"Missing required options {base_url}, {upd_pfx}, {dl_dir}"
+            )
+
+        os.makedirs(dl_dir, exist_ok=True)
+        if not os.path.isdir(dl_dir):
+            raise ValueError(f"Output directory does not exist {dl_dir}")
+
+        """
+        When downloading updates from route-views, we calculate the name of
+        the last complete 15 minute update file. At 09.13 the last complete
+        update file will be called "0845", it will be for the period
+        08.45-09.00. So the filename can be calculatd as:
+        "round down to the last 15 minute whole interval - another 15 minutes".
+
+        If the current time is 09.15 (a round 15 minute interval) a dump
+        should be available called "0900" for 09.00-09.15 period. The archive
+        might be slow to update though.
+
+        To be safe:
+        At 09.13 this function will download the "0845" file (08.45-09.00).
+        At 09.15 this function will download the "0845" file too.
+        At 09.17 this function will download the "0900" file (09.00-09.15).
+
+        If this machine is in a different timezone to the archive server, an
+        additional offset is required, RV_UPD_OFFSET.
+        """
+        minutes = int(datetime.datetime.strftime(datetime.datetime.now(), "%M"))
+        mod = minutes % 15
+        if mod == 0:
+            m_delta = datetime.timedelta(minutes=30)
+        else:
+            m_delta = datetime.timedelta(minutes=(15 + mod))
+
+        h_delta = datetime.timedelta(hours=cfg.RV_UPD_OFFSET)
+
+        ym = datetime.datetime.strftime(datetime.datetime.now()-h_delta,"%Y.%m")
+        ymd_hm = datetime.datetime.strftime(datetime.datetime.now()-h_delta-m_delta,"%Y%m%d.%H%M")
+
+        udp_url = base_url + ym + upd_pfx + ymd_hm + file_ext
+        filename = dl_dir + os.path.basename(udp_url)
+
+        if (not replace and os.path.exists(filename)):
+                logging.info(f"Skipping existing file {filename}")
+        else:
+            mrt_getter.download_mrt(filename=filename, url=udp_url)
+
+        return (filename, udp_url)
+
+    @staticmethod
+    def get_range_rv_upd(
+        base_url=None,
+        dl_dir=None,
+        end_date=None,
+        file_ext=None,
+        replace=False,
+        start_date=None,
+        upd_pfx=None,
+    ):
+
+        """
+        Download a range of MRT update dump files from route-views archive.
+        All update MRT files from and inclusive of start_date to and inclusive
+        of end_date.
+
+        start_date: In the MRT date format yyyymmdd.hhmm "20220129.0915"
+        end_date: In the MRT date format yyyymmdd.hhmm "20220129.0915"
+        """
+
+        if (not start_date or not end_date):
+            raise ValueError(
+                f"Missing required options {start_date}, {end_date}"
+            )
+
+        start = datetime.datetime.strptime(start_date, "%Y%m%d.%H%M")
+        end = datetime.datetime.strptime(end_date, "%Y%m%d.%H%M")
+
+        if end < start:
+            raise ValueError(
+                f"End date is before start date {start_date}, {end_date}"
+            )
+
+        diff = end - start
+        count = diff.seconds // 900 # RV updates are every 15 minutes
+        downloaded = []
+
+        for i in range(0, count + 1):
             m_delta = datetime.timedelta(minutes=(i*15))
-            update_ymd_hm = datetime.datetime.strftime(update_base+m_delta,"%Y%m%d.%H%M")
+            ym = datetime.datetime.strftime(start+m_delta,"%Y.%m")
+            ymd_hm = datetime.datetime.strftime(start+m_delta,"%Y%m%d.%H%M")
 
-            latest_linx_upd = mrt_getter.RV_LINX + latest_ym + "/UPDATES/updates." + update_ymd_hm + ".bz2"
-            filename = "/tmp/" + os.path.basename(latest_linx_upd)
-            mrt_getter.download_mrt(filename=filename, url=latest_linx_upd)
-            downloaded.append(filename)
+            udp_url = base_url + ym + upd_pfx + ymd_hm + file_ext
+            filename = dl_dir + os.path.basename(udp_url)
+
+            if (not replace and os.path.exists(filename)):
+                    logging.info(f"Skipping existing file {filename}")
+            else:
+                mrt_getter.download_mrt(filename=filename, url=udp_url)
+                downloaded.append((filename, udp_url))
 
         return downloaded
+
 
     @staticmethod
     def download_mrt(filename=None, url=None, debug=False):
@@ -92,12 +332,12 @@ class mrt_getter:
             filename = os.path.basename(url)
 
         with open(filename, "wb") as f:
-            print(f"Downloading {url} to {filename}")
+            logging.info(f"Downloading {url} to {filename}")
             try:
                 req = requests.get(url, stream=True)
             except requests.exceptions.ConnectionError as e:
                 if debug:
-                    print(f"Couldn't connect to MRT server: {e}")
+                    logging.debug(f"Couldn't connect to MRT server: {e}")
                 f.close()
                 raise requests.exceptions.ConnectionError
 
@@ -105,32 +345,32 @@ class mrt_getter:
 
             if req.status_code != 200:
                 if debug:
-                    print(f"HTTP error: {req.status_code}")
-                    print(req.url)
-                    print(req.text)
+                    logging.debug(f"HTTP error: {req.status_code}")
+                    logging.debug(req.url)
+                    logging.debug(req.text)
                 f.write(req.content)
                 f.close()
                 req.raise_for_status()
 
             if file_len is None:
                 if debug:
-                    print(req.url)
-                    print(req.text)
+                    logging.debug(req.url)
+                    logging.debug(req.text)
                 f.write(req.content)
                 f.close()
                 raise ValueError("Missing file length!")
 
             file_len = int(file_len)
             rcvd = 0
-            print(f"File size is {file_len/1024/1024:.7}MBs")
+            logging.info(f"File size is {file_len/1024/1024:.7}MBs")
             progress = 0
             for chunk in req.iter_content(chunk_size=1024):
 
                 if req.status_code != 200:
                     if debug:
-                        print(f"HTTP error: {req.status_code}")
-                        print(req.url)
-                        print(req.text)
+                        logging.debug(f"HTTP error: {req.status_code}")
+                        logging.debug(req.url)
+                        logging.debug(req.text)
                     f.write(req.content)
                     f.close()
                     req.raise_for_status()
