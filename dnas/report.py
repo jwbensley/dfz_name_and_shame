54e9c7f78d5106c3ce6c6e2e77a34f5c4f30af7f
james.bensley <james.bensley@sky.uk>
2022-03-23 21:45:06 +0100

    Fixing issue #70 by adding support for reports which are pushed to git

---
+++ dnas/report.py
@@ -0,0 +1,264 @@
+import datetime
+import logging
+from typing import List
+
+from dnas.config import config as cfg
+from dnas.mrt_stats import mrt_stats
+from dnas.whois import whois
+
+class report:
+    """
+    Class for generating human readable text reports from the stats stored in
+    Redis.
+    """
+
+    @staticmethod
+    def gen_txt_report(mrt_s: 'mrt_stats' = None) -> List[str]:
+        """
+        Generate a text report using the data in an mrt stats object.
+        """
+        if not mrt_s:
+            raise ValueError(
+                f"Missing required arguments: mrt_s={mrt_s}"
+            )
+
+        if type(mrt_s) != mrt_stats:
+            raise TypeError(
+                f"mrt_s is not an mrt_s object: {type(mrt_s)}"
+            )
+
+        # Reduce the load on WHOIS by caching responses. It's also faster :)
+        whois_cache = {}
+        report = []
+
+        if mrt_s.total_upd:
+            text = (
+                f"On {mrt_s.ts_ymd_format()} {mrt_s.total_upd} BGP UPDATES "
+                f"were parsed. {mrt_s.total_advt} UPDATES contained prefix "
+                f"advertisements. {mrt_s.total_withd} UPDATES contained prefix "
+                f"withdraws.\n\n"
+            )
+
+            report.append(text)
+
+        if mrt_s.longest_as_path:
+            text = (
+                f"Longest AS path: {len(mrt_s.longest_as_path)} prefix(es) had "
+                f"an AS path length of {len(mrt_s.longest_as_path[0].as_path)} "
+                f"ASNs.\n"
+            )
+
+            for mrt_e in mrt_s.longest_as_path:
+                text += f"{mrt_e.prefix} from origin ASN(s)"
+                for asn in mrt_e.origin_asns:
+                    if asn not in whois_cache:
+                        whois_cache[asn] = whois.as_lookup(int(asn))
+                    as_name = whois_cache[asn]
+                    if as_name:
+                        text += f" AS{asn} ({as_name})"
+                    else:
+                        text += f" AS{asn}"
+                text += f". AS Path: AS{' AS'.join(mrt_e.as_path)}. "
+            text = text[0:-1]
+            text += "\n\n"
+            report.append(text)
+
+        if mrt_s.longest_comm_set:
+            text = (
+                f"Longest community set: {len(mrt_s.longest_comm_set)} "
+                f"prefix(es) had a community set length of "
+                f"{len(mrt_s.longest_comm_set[0].comm_set)} communities.\n"
+            )
+
+            for mrt_e in mrt_s.longest_comm_set:
+                text += f"{mrt_e.prefix} from origin ASN(s)"
+                for asn in mrt_e.origin_asns:
+                    if asn not in whois_cache:
+                        whois_cache[asn] = whois.as_lookup(int(asn))
+                    as_name = whois_cache[asn]
+                    if as_name:
+                        text += f" AS{asn} ({as_name})"
+                    else:
+                        text += f" AS{asn}"
+                text += f". Commnuity set: {' '.join(mrt_e.comm_set)}.\n"
+            text = text[0:-1]
+            text += "\n\n"
+            report.append(text)
+
+        if mrt_s.most_advt_prefixes:
+            text = (
+                f"Most BGP advertisements per prefix: "
+                f"{len(mrt_s.most_advt_prefixes)} prefix(es) had "
+                f"{mrt_s.most_advt_prefixes[0].advt} advertisements.\n"
+            )
+
+            text += "Prefix(es):"
+            for mrt_e in mrt_s.most_advt_prefixes:
+                text += f" {mrt_e.prefix}"
+            text += "\n\n"
+
+            report.append(text)
+
+        if mrt_s.most_upd_prefixes:
+            text = (
+                f"Most BGP updates per prefix: "
+                f"{len(mrt_s.most_upd_prefixes)} prefix(es) had "
+                f"{mrt_s.most_upd_prefixes[0].updates} updates.\n"
+            )
+
+            text += "Prefix(es):"
+            for mrt_e in mrt_s.most_upd_prefixes:
+                text += f" {mrt_e.prefix}"
+            text += "\n\n"
+
+            report.append(text)
+
+        if mrt_s.most_withd_prefixes:
+            text = (
+                f"Most BGP withdraws per prefix: "
+                f"{len(mrt_s.most_withd_prefixes)} prefix(es) had "
+                f"{mrt_s.most_withd_prefixes[0].withdraws} withdraws.\n"
+            )
+
+            text += "Prefix(es):"
+            for mrt_e in mrt_s.most_withd_prefixes:
+                text += f" {mrt_e.prefix}"
+            text += "\n\n"
+
+            report.append(text)
+
+        if mrt_s.most_advt_origin_asn:
+            text = (
+                f"Most BGP advertisements per origin ASN: "
+                f"{len(mrt_s.most_advt_origin_asn)} origin ASN(s) sent "
+                f"{mrt_s.most_advt_origin_asn[0].advt} advertisements.\n"
+            )
+
+            text += "Origin ASN(s):"
+            for mrt_e in mrt_s.most_advt_origin_asn:
+                for asn in mrt_e.origin_asns:
+                    if asn not in whois_cache:
+                        whois_cache[asn] = whois.as_lookup(int(asn))
+                    as_name = whois_cache[asn]
+                    if as_name:
+                        text += f" AS{asn} ({as_name})"
+                    else:
+                        text += f" AS{asn}"
+                text += ", "
+            text = text[0:-2]
+            text += "\n\n"
+
+            report.append(text)
+
+        if mrt_s.most_advt_peer_asn:
+            text = (
+                f"Most BGP advertisements per peer ASN: "
+                f"{len(mrt_s.most_advt_peer_asn)} peer ASN(s) sent "
+                f"{mrt_s.most_advt_peer_asn[0].advt} advertisements.\n"
+            )
+
+            text += "Peer ASN(s):"
+            for mrt_e in mrt_s.most_advt_peer_asn:
+                if mrt_e.peer_asn not in whois_cache:
+                    whois_cache[mrt_e.peer_asn] = whois.as_lookup(int(mrt_e.peer_asn))
+                as_name = whois_cache[mrt_e.peer_asn]
+                if as_name:
+                    text += f" AS{mrt_e.peer_asn} ({as_name})"
+                else:
+                    text += f" AS{mrt_e.peer_asn}"
+                text += ", "
+            text = text[0:-2]
+            text += "\n\n"
+
+            report.append(text)
+
+        if mrt_s.most_upd_peer_asn:
+            text = (
+                f"Most BGP updates per peer ASN: "
+                f"{len(mrt_s.most_upd_peer_asn)} peer ASN(s) sent "
+                f"{mrt_s.most_upd_peer_asn[0].updates} updates.\n"
+            )
+
+            text += "Peer ASN(s):"
+            for mrt_e in mrt_s.most_upd_peer_asn:
+                if mrt_e.peer_asn not in whois_cache:
+                    whois_cache[mrt_e.peer_asn] = whois.as_lookup(int(mrt_e.peer_asn))
+                as_name = whois_cache[mrt_e.peer_asn]
+                if as_name:
+                    text += f" AS{mrt_e.peer_asn} ({as_name})"
+                else:
+                    text += f" AS{mrt_e.peer_asn}"
+                text += ", "
+            text = text[0:-2]
+            text += "\n\n"
+
+            report.append(text)
+
+        if mrt_s.most_withd_peer_asn:
+            text = (
+                f"Most BGP withdraws per peer ASN: "
+                f"{len(mrt_s.most_withd_peer_asn)} peer ASN(s) sent "
+                f"{mrt_s.most_withd_peer_asn[0].withdraws} withdraws.\n"
+            )
+
+            text += "Peer ASN(s):"
+            for mrt_e in mrt_s.most_withd_peer_asn:
+                if mrt_e.peer_asn not in whois_cache:
+                    whois_cache[mrt_e.peer_asn] = whois.as_lookup(int(mrt_e.peer_asn))
+                as_name = whois_cache[mrt_e.peer_asn]
+                if as_name:
+                    text += f" AS{mrt_e.peer_asn} ({as_name})"
+                else:
+                    text += f" AS{mrt_e.peer_asn}"
+                text += ", "
+            text = text[0:-2]
+            text += "\n\n"
+
+            report.append(text)
+
+        if mrt_s.most_origin_asns:
+            text = (
+                f"Most origin ASNs per prefix: "
+                f"{len(mrt_s.most_origin_asns)} prefix(es) had "
+                f"{len(mrt_s.most_origin_asns[0].origin_asns)} origin ASNs.\n"
+            )
+
+            text += "Prefix(es): "
+            for mrt_e in mrt_s.most_origin_asns:
+                text += f"{mrt_e.prefix}"
+                for asn in mrt_e.origin_asns:
+                    if asn not in whois_cache:
+                        whois_cache[asn] = whois.as_lookup(int(asn))
+                    as_name = whois_cache[asn]
+                    if as_name:
+                        text += f" AS{asn} ({as_name})"
+                    else:
+                        text += f" AS{asn}"
+                text += "\n"
+            text = text[0:-1]
+            text += "\n\n"
+
+            report.append(text)
+
+        return report
+
+    @staticmethod
+    def gen_txt_report_fn_ymd(ymd: str = None) -> str:
+        """
+        Generate and return the filename for the text file report for a
+        specific date.
+        """
+        if not ymd:
+            raise ValueError(
+                f"Missing required arguments: ymd={ymd}."
+            )
+
+        if type(ymd) != str:
+            raise TypeError(
+                f"ymd is not a string: {type(ymd)}"
+            )
+
+        # This is just a crude check that "ymd" is in the correct format
+        day = datetime.datetime.strptime(ymd, cfg.DAY_FORMAT)
+
+        return f"{ymd}.txt"
