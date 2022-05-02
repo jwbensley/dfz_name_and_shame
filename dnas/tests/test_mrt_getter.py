7c912907dbc0f66c8bf441bb8575238674bf7161
jwbensley <jwbensley@gmail.com>
2022-05-02 17:59:21 +0200

    Finishing unittest for mrt_archive

--- dnas/tests/test_mrt_getter.py
+++ dnas/tests/test_mrt_getter.py
@@ -1,10 +1,16 @@
-import unittest
 import os
 import requests
 import sys
-sys.path.append('./')
-from mrt_getter import mrt_getter
-from mrt_parser import mrt_parser
+import unittest
+
+sys.path.append(
+    os.path.join(
+        os.path.dirname(os.path.realpath(__file__))
+        , "../"
+    )
+)
+from dnas.mrt_getter import mrt_getter
+from dnas.mrt_parser import mrt_parser
 
 
 class test_mrt_splitter(unittest.TestCase):
@@ -21,13 +27,29 @@ class test_mrt_splitter(unittest.TestCase):
         with self.assertRaises(ValueError):
             mrt_getter.download_mrt()
         with self.assertRaises(PermissionError):
-            mrt_getter.download_mrt(self.bad_output_file, self.linx_update_url)
+            mrt_getter.download_mrt(
+                filename = self.bad_output_file,
+                replace = False,
+                url = self.linx_update_url
+            )
         with self.assertRaises(requests.exceptions.HTTPError):
-            mrt_getter.download_mrt(self.output_file, self.invalid_path)
+            mrt_getter.download_mrt(
+                filename = self.output_file,
+                replace = False,
+                url = self.invalid_path
+            )
         with self.assertRaises(requests.exceptions.ConnectionError):
-            mrt_getter.download_mrt(self.output_file, self.invalid_domain)
+            mrt_getter.download_mrt(
+                filename = self.output_file,
+                replace = False,
+                url = self.invalid_domain
+            )
         
-        mrt_getter.download_mrt(self.output_file, self.linx_update_url, True)
+        mrt_getter.download_mrt(
+            filename = self.output_file,
+            replace = False,
+            url = self.linx_update_url
+        )
         self.assertTrue(os.path.isfile(self.output_file))
         self.assertEqual(self.no_records, mrt_parser.mrt_count(self.output_file))
 
