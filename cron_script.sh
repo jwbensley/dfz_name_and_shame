067dcc4859354cf66f00f1853c1f7ed9612e599f
jwbensley <1025875+jwbensley@users.noreply.github.com>
2022-05-08 08:48:56 +0200

    Tidy file/folder structure

--- cron_script.sh
+++ cron_script.sh
@@ -11,16 +11,43 @@ set -e
 
 
 # Check for any MRTs the continuous getter missed:
-docker run -it --rm -v /etc/localtime:/etc/localtime -v /media/usb0/:/media/usb0/ -v /opt/dnas/secrets/redis_auth.py:/opt/dnas/dnas/dnas/redis_auth.py --name dnas_tmp dnas:latest /opt/pypy3.8-v7.3.7-aarch64/bin/pypy3 /opt/dnas/dnas/scripts/get_mrts.py --yesterday --update --enabled
+docker run -it --rm \
+-v /etc/localtime:/etc/localtime \
+-v /media/usb0/:/media/usb0/ \
+--name dnas_tmp \
+dnas:latest \
+/opt/pypy3.8-v7.3.7-aarch64/bin/pypy3 /opt/dnas/scripts/get_mrts.py --yesterday --update --enabled
 
 # Parse any missing MRTs that were downloaded:
-docker run -it --rm -v /etc/localtime:/etc/localtime -v /media/usb0/:/media/usb0/ -v /opt/dnas/secrets/redis_auth.py:/opt/dnas/dnas/dnas/redis_auth.py --name dnas_tmp dnas:latest /opt/pypy3.8-v7.3.7-aarch64/bin/pypy3 /opt/dnas/dnas/scripts/parse_mrts.py --yesterday --update --remove --enabled
+docker run -it --rm \
+-v /etc/localtime:/etc/localtime \
+-v /media/usb0/:/media/usb0/ \
+--name dnas_tmp \
+dnas:latest \
+/opt/pypy3.8-v7.3.7-aarch64/bin/pypy3 /opt/dnas/scripts/parse_mrts.py --yesterday --update --remove --enabled
 
 # Generate the daily stats for yesterday:
-docker run -it --rm -v /etc/localtime:/etc/localtime -v /media/usb0/:/media/usb0/ -v /opt/dnas/secrets/redis_auth.py:/opt/dnas/dnas/dnas/redis_auth.py --name dnas_tmp dnas:latest /opt/pypy3.8-v7.3.7-aarch64/bin/pypy3 /opt/dnas/dnas/scripts/stats.py --yesterday --update --enabled
+docker run -it --rm \
+-v /etc/localtime:/etc/localtime \
+-v /media/usb0/:/media/usb0/ \
+--name dnas_tmp \
+dnas:latest \
+/opt/pypy3.8-v7.3.7-aarch64/bin/pypy3 /opt/dnas/scripts/stats.py --yesterday --update --enabled
 
 # Generate the text report from the generated stats:
-docker run -it --rm -v /etc/localtime:/etc/localtime -v /media/usb0/:/media/usb0/ -v /opt/dnas/secrets/redis_auth.py:/opt/dnas/dnas/dnas/redis_auth.py -v /opt/dnas/secrets/ed25519:/root/.ssh/id_ed25519 -v /opt/dnas/secrets/ed25519.pub:/root/.ssh/id_ed25519.pub -e LANG=C.UTF-8 -e LC_ALL=C.UTF-8 --name dnas_tmp dnas:latest /opt/pypy3.8-v7.3.7-aarch64/bin/pypy3 /opt/dnas/dnas/scripts/git_reports.py --yesterday
+docker run -it --rm \
+-v /etc/localtime:/etc/localtime \
+-v /media/usb0/:/media/usb0/ \
+-e LANG=C.UTF-8 -e LC_ALL=C.UTF-8 \
+--name dnas_tmp \
+dnas:latest \
+/opt/pypy3.8-v7.3.7-aarch64/bin/pypy3 /opt/dnas/scripts/git_reports.py --yesterday
 
 # Tweet the report summary and link to full report:
-docker run -it --rm -v /etc/localtime:/etc/localtime -v /media/usb0/:/media/usb0/ -v /opt/dnas/secrets/redis_auth.py:/opt/dnas/dnas/dnas/redis_auth.py -v /opt/dnas/secrets/twitter_auth.py:/opt/dnas/dnas/dnas/twitter_auth.py --name dnas_tmp dnas:latest /opt/pypy3.8-v7.3.7-aarch64/bin/pypy3 /opt/dnas/dnas/scripts/tweet.py --yesterday
+docker run -it --rm \
+-v /etc/localtime:/etc/localtime \
+-v /media/usb0/:/media/usb0/ \
+--name dnas_tmp \
+dnas:latest \
+/opt/pypy3.8-v7.3.7-aarch64/bin/pypy3 /opt/dnas/scripts/tweet.py --yesterday
+
