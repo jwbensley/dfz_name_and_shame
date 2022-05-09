a391efba3c016a432e1d796d4720dcc262dd7f39
james.bensley <1025875+jwbensley@users.noreply.github.com>
2022-05-09 09:34:51 +0200

    #58 adding support for short/long prefix detection

--- dnas/dnas/report.py
+++ dnas/dnas/report.py
@@ -146,6 +146,33 @@ class report:
                 text += "\n\n"
                 txt_report.append(text)
 
+        if mrt_s.invalid_len:
+            text = (
+                f"Abnormally large/small prefixes with most origin ASNs per prefix: "
+                f"{len(mrt_s.invalid_len)} large/small prefix(es) had "
+                f"{len(mrt_s.invalid_len[0].origin_asns)} origin ASNs.\n"
+            )
+
+            txt_report.append(text)
+
+            if body:
+                text = ""
+                for mrt_e in mrt_s.invalid_len:
+                    text += f"Prefix {mrt_e.prefix} from origin ASN(s)"
+                    for asn in mrt_e.origin_asns:
+                        if asn not in whois_cache:
+                            whois_cache[asn] = whois.as_lookup(int(asn))
+                        as_name = whois_cache[asn]
+                        if as_name:
+                            text += f" AS{asn} ({as_name})"
+                        else:
+                            text += f" AS{asn}"
+                    text += "\n"
+                text = text[0:-1]
+                text += "\n\n"
+
+                txt_report.append(text)
+
         if mrt_s.most_advt_prefixes:
             text = (
                 f"Most BGP advertisements per prefix: "
