1034133a45f8169473ba97b272df95be0b3ad33c
jwbensley <jwbensley@gmail.com>
2022-05-17 09:17:18 +0200

    Rollback to 5482d1d78fddc16067bcf99ef55b73074a263af9

--- dnas/tests/test_mrt_splitter.py
+++ dnas/tests/test_mrt_splitter.py
@@ -18,24 +18,26 @@ from dnas.mrt_parser import mrt_parser
 
 class test_mrt_splitter(unittest.TestCase):
 
-    no_of_chunks = 8
-    gz_filename = "tests/rcc23.updates.20220501.2305.gz"
-    file_size = 30285
+
+    def setUp(self):
+        self.no_of_chunks = 8
+        self.gz_filename = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            "rcc23.updates.20220501.2305.gz"
+        )
+        self.file_size = 30285
 
     def test_init(self):
-
-        with self.assertRaises(TypeError):
-            mrt_splitter(self.gz_filename, 1.23)
-        with self.assertRaises(ValueError):
-            mrt_splitter("")
-        with self.assertRaises(FileNotFoundError):
-            mrt_splitter("PewjWxSQavM7tCQbXIZlgcK9zXfr1H")
+        self.assertRaises(ValueError, mrt_splitter, "")
+        self.assertRaises(TypeError, mrt_splitter, 1.23)
+        self.assertRaises(FileNotFoundError, mrt_splitter, "PewjWxSQavM7tCQbXIZlgcK9zXfr1H")
 
         splitter = mrt_splitter(self.gz_filename)
         self.assertTrue(isinstance(splitter, mrt_splitter))
         self.assertTrue(isinstance(splitter.f, gzip.GzipFile))
         self.assertTrue(isinstance(splitter.filename, str))
         self.assertTrue(splitter.filename, True)
+        self.assertEqual(splitter.filename, self.gz_filename)
         try:
             splitter.close()
         except StopIteration:
@@ -44,8 +46,8 @@ class test_mrt_splitter(unittest.TestCase):
     def test_split(self):
 
         splitter = mrt_splitter(self.gz_filename)
-        with self.assertRaises(ValueError):
-            splitter.split(-1)
+
+        self.assertRaises(ValueError, splitter.split, -1)
         total, chunk_names = splitter.split(self.no_of_chunks)
 
         self.assertTrue(isinstance(total, int))
@@ -58,7 +60,13 @@ class test_mrt_splitter(unittest.TestCase):
             self.assertTrue(os.path.isfile(filename))
             mrt_count += mrt_parser.mrt_count(filename)
         self.assertEqual(total, mrt_count)
+        try:
+            splitter.close()
+        except StopIteration:
+            pass
 
+        for filename in chunk_names:
+            os.unlink(filename)
 
 if __name__ == '__main__':
     unittest.main()
