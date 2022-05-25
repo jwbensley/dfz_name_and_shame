6f4fd0fbba441aaf46f799c0730a56fce9e399d8
james.bensley <james.bensley@sky.uk>
2022-05-25 13:53:32 +0200

    Update docker scripts to use new base directory

--- docker/cron_script.sh
+++ docker/cron_script.sh
@@ -9,13 +9,14 @@ set -o pipefail
 # Error if any command returns a non-zero exist status
 set -e
 
+base_dir="/opt/dnas_data/"
 #pypy="/opt/pypy3.8-v7.3.7-aarch64/bin/pypy3"
 pypy="/opt/pypy3.8-v7.3.7-linux64/bin/pypy3"
 
 # Check for any MRTs the continuous getter missed:
 docker run -t --rm \
 -v /etc/localtime:/etc/localtime \
--v /media/usb0/:/media/usb0/ \
+-v "$base_dir":"$base_dir" \
 --name dnas_tmp \
 dnas:latest \
 "$pypy" /opt/dnas/scripts/get_mrts.py --backfill --yesterday --update --enabled
@@ -23,7 +24,7 @@ dnas:latest \
 # Parse any missing MRTs that were downloaded:
 docker run -t --rm \
 -v /etc/localtime:/etc/localtime \
--v /media/usb0/:/media/usb0/ \
+-v "$base_dir":"$base_dir" \
 --name dnas_tmp \
 dnas:latest \
 "$pypy" /opt/dnas/scripts/parse_mrts.py --yesterday --update --remove --enabled
@@ -31,7 +32,7 @@ dnas:latest \
 # Generate the daily stats for yesterday:
 docker run -t --rm \
 -v /etc/localtime:/etc/localtime \
--v /media/usb0/:/media/usb0/ \
+-v "$base_dir":"$base_dir" \
 --name dnas_tmp \
 dnas:latest \
 "$pypy" /opt/dnas/scripts/stats.py --yesterday --update --enabled
@@ -39,7 +40,7 @@ dnas:latest \
 # Generate the text report from the generated stats:
 docker run -t --rm \
 -v /etc/localtime:/etc/localtime \
--v /media/usb0/:/media/usb0/ \
+-v "$base_dir":"$base_dir" \
 -e LANG=C.UTF-8 -e LC_ALL=C.UTF-8 \
 --name dnas_tmp \
 dnas:latest \
@@ -48,7 +49,7 @@ dnas:latest \
 # Tweet the report summary and link to full report:
 docker run -t --rm \
 -v /etc/localtime:/etc/localtime \
--v /media/usb0/:/media/usb0/ \
+-v "$base_dir":"$base_dir" \
 --name dnas_tmp \
 dnas:latest \
 "$pypy" /opt/dnas/scripts/tweet.py --yesterday
--- docker/day.sh
+++ docker/day.sh
@@ -13,33 +13,34 @@ set -o pipefail
 # Error if any command returns a non-zero exist status
 set -e
 
+base_dir="/opt/dnas_data/"
 #pypy="/opt/pypy3.8-v7.3.7-aarch64/bin/pypy3"
 pypy="/opt/pypy3.8-v7.3.7-linux64/bin/pypy3"
 
 docker run -it --rm \
 -v /etc/localtime:/etc/localtime \
--v /media/usb0/:/media/usb0/ \
+-v "$base_dir":"$base_dir" \
 --name dnas_tmp \
 dnas:latest \
 "$pypy" /opt/dnas/scripts/get_mrts.py --backfill --update --enabled --range --start "$1".0000 --end "$1".2359
 
 docker run -it --rm \
 -v /etc/localtime:/etc/localtime \
--v /media/usb0/:/media/usb0/ \
+-v "$base_dir":"$base_dir" \
 --name dnas_tmp \
 dnas:latest \
 "$pypy" /opt/dnas/scripts/parse_mrts.py --update --remove --enabled --ymd "$1"
 
 docker run -it --rm \
 -v /etc/localtime:/etc/localtime \
--v /media/usb0/:/media/usb0/ \
+-v "$base_dir":"$base_dir" \
 --name dnas_tmp \
 dnas:latest \
 "$pypy" /opt/dnas/scripts/stats.py --update --enabled --daily --ymd "$1"
 
 docker run -it --rm \
 -v /etc/localtime:/etc/localtime \
--v /media/usb0/:/media/usb0/ \
+-v "$base_dir":"$base_dir" \
 -e LANG=C.UTF-8 -e LC_ALL=C.UTF-8 \
 --name dnas_tmp \
 dnas:latest \
@@ -47,7 +48,7 @@ dnas:latest \
 
 docker run -it --rm \
 -v /etc/localtime:/etc/localtime \
--v /media/usb0/:/media/usb0/ \
+-v "$base_dir":"$base_dir" \
 --name dnas_tmp \
 dnas:latest \
 "$pypy" /opt/dnas/scripts/tweet.py --generate --tweet --ymd "$1"
