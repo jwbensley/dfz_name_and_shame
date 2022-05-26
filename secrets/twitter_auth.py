970657cea82ef59650cec2ae788f4aa8c1380f95
james.bensley <james.bensley@sky.uk>
2022-05-26 09:32:19 +0200

    Remove secrets

--- secrets/twitter_auth.py
+++ secrets/twitter_auth.py
@@ -2,5 +2,5 @@
 class twitter_auth:
     consumer_key = "ABC123"
     consumer_secret = "ABC123"
-    access_token = "1477316189462347780-ABC123"
+    access_token = "ABC123"
     access_token_secret = "ABC123"
