672dd393e340b38f94654d79f88196febec556f3
James Bensley <1025875+jwbensley@users.noreply.github.com>
2022-06-13 10:14:25 +0200

    Adding whois unit tests for issue #55 (#146)
    * Adding whois unit tests
    
    * Adding whois unit tests
    
    * Adding whois unit tests
    
    * Adding whois unit tests
    
    Co-authored-by: jwbensley <jwbensley@gmail.com>

--- dnas/tests/test_mrt_parser.py
+++ dnas/tests/test_mrt_parser.py
@@ -22,7 +22,7 @@ class test_mrt_parser(unittest.TestCase):
 
     def setUp(self):
         """
-        Copy the test files to the location that would be in,
+        Copy the test files to the location they would be in,
         if we had downloaded them from the public archives:
         """
 
