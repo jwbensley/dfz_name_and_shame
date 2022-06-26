5d663839b04503f73bcc00e24cdae1460fe1e069
jwbensley <1025875+jwbensley@users.noreply.github.com>
2022-06-26 20:33:27 +0200

    Update hostname in example redis_auth.py file to show that the docker-container name should be used

--- secrets/redis_auth.py
+++ secrets/redis_auth.py
@@ -1,5 +1,6 @@
 
 class redis_auth():
-    host = "172.17.0.1" # DOCKER IP
+    #host = "172.17.0.1" # DOCKER IP
+    host = "dnas_redis" # docker-compose container name
     port = 6379
     password = "REDIS.CONF-PASSWORD-GOES-HERE"
