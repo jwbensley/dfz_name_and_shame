04c5ff1fb1038f731d0dc3b84c9f5e47cc648bac
James Bensley <jwbensley@users.noreply.github.com>
2023-07-02 13:35:57 +0200

    Fix issue #161 by adding existing script to git repo (#174)
    Co-authored-by: James Bensley <jwbensley@gmail.com>

---
+++ redis/backup_redis.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+
+set -eu
+
+datetime=$(date "+%Y-%m-%d")
+backup_dir="/home/bensley/backups/redis"
+backup_file="${backup_dir}/redis-${datetime}.json"
+
+mkdir -p "${backup_dir}"
+chown bensley:bensley "${backup_dir}"
+source /opt/dnas/venv/bin/activate
+cd /opt/dnas/dnas/scripts/ || exit 1
+./redis_mgmt.py --dump "${backup_file}"
+gzip "${backup_file}"
