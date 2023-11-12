d9fde9584868c312a33594004baa88bb97fa4fd4
James Bensley <jwbensley@gmail.com>
2023-11-12 20:25:31 +0100

    Issue-#132 Add --no-compression option

--- dnas/dnas/redis_db.py
+++ dnas/dnas/redis_db.py
@@ -91,7 +91,9 @@ class redis_db:
 
         return self.r.delete(key)
 
-    def from_file(self: "redis_db", filename: str) -> None:
+    def from_file(
+        self: "redis_db", filename: str, compression: bool = True
+    ) -> None:
         """
         Restore redis DB from JSON file.
         """
@@ -103,7 +105,9 @@ class redis_db:
         with open(filename, "r") as f:
             self.from_json(f.read())
 
-    def from_file_stream(self: "redis_db", filename: str) -> None:
+    def from_file_stream(
+        self: "redis_db", filename: str, compression: bool = True
+    ) -> None:
         """
         Restore redis DB from JSON file, loading the content one line at time.
         """
@@ -173,12 +177,12 @@ class redis_db:
                 # Load it into REDIS
                 k = list(json_dict.keys())[0]
                 v = list(json_dict.values())[0]
-                self.r.set(k, redis_db.compress(v))
+                self.set(key=k, value=v, compression=compression)
                 loaded_kvs += 1
 
         logging.info(f"Loaded {loaded_kvs} k/v's from stream")
 
-    def from_json(self: "redis_db", json_str: str):
+    def from_json(self: "redis_db", json_str: str, compression: bool = True):
         """
         Restore redis DB from a JSON string
         """
@@ -189,10 +193,12 @@ class redis_db:
 
         json_dict = json.loads(json_str)
         for k in json_dict.keys():
-            self.r.set(k, redis_db.compress(json_dict[k]))
+            self.set(key=k, value=json_dict[k], compression=compression)
         logging.info(f"Loaded {len(json_dict)} k/v's")
 
-    def get(self: "redis_db", key: str) -> Union[str, list]:
+    def get(
+        self: "redis_db", key: str, compression: bool = True
+    ) -> Union[str, list]:
         """
         Return the value stored in "key" from Redis
         """
@@ -203,16 +209,22 @@ class redis_db:
         if t == "string":
             val = self.r.get(key)
             if val:
-                return self.decompress(val.decode("utf-8"))
+                if compression:
+                    return redis_db.decompress(val.decode("utf-8"))
+                else:
+                    return val.decode("utf-8")
             else:
                 raise ValueError(
                     f"Couldn't decode data stored under key {key}"
                 )
         elif t == "list":
-            return [
-                redis_db.compress(x.decode("utf-8"))
-                for x in self.r.lrange(key, 0, -1)
-            ]
+            if compression:
+                return [
+                    redis_db.compress(x.decode("utf-8"))
+                    for x in self.r.lrange(key, 0, -1)
+                ]
+            else:
+                return [x.decode("utf-8") for x in self.r.lrange(key, 0, -1)]
         else:
             raise TypeError(f"Unknown redis data type stored under {key}: {t}")
 
@@ -248,7 +260,9 @@ class redis_db:
 
         return msgs
 
-    def get_stats(self: "redis_db", key: str) -> Union[None, "mrt_stats"]:
+    def get_stats(
+        self: "redis_db", key: str, compression: bool = True
+    ) -> Union[None, "mrt_stats"]:
         """
         Return MRT stats from Redis as JSON, and return as an MRT stats object.
         """
@@ -256,42 +270,35 @@ class redis_db:
             raise ValueError(f"Missing required arguments: key={key}")
 
         mrt_s = mrt_stats()
-        json_str = self.r.get(key)
+        json_str = self.get(key, compression=compression)
+        assert type(json_str) == str
         if not json_str:
             return None
         else:
-            mrt_s.from_json(redis_db.decompress(json_str.decode("utf-8")))
+            if compression:
+                mrt_s.from_json(redis_db.decompress(json_str))
+            else:
+                mrt_s.from_json(json_str)
             return mrt_s
 
     def ping(self: "redis_db") -> None:
         assert self.r.ping()
 
-    def set_stats(self: "redis_db", key: str, mrt_s: "mrt_stats"):
+    def set(self: "redis_db", key: str, value: str, compression: bool = True):
         """
-        Take an MRT stats object, serialise it to JSON, store in Redis.
+        Take a key and a string and store it in Redis.
         """
-        if not key or not mrt_s:
+        if not key or not value:
             raise ValueError(
-                f"Missing required arguments: key={key}, mrt_s={mrt_s}"
+                f"Missing required arguments: key={key}, value={value}"
             )
 
-        self.r.set(key, redis_db.compress(mrt_s.to_json()))
+        if compression:
+            self.r.set(key, redis_db.compress(value))
+        else:
+            self.r.set(key, value)
 
-    def set_stats_json(self: "redis_db", key: str, json_str: str):
-        """
-        Take JSON serialisation of an MRT stats object, and store in Redis.
-        """
-        if not key:
-            raise ValueError(f"Missing required arguments: key={key}")
-
-        if not json_str:
-            raise ValueError(
-                f"Missing required arguments: json_str={json_str}"
-            )
-
-        self.r.set(key, redis_db.compress(json_str))
-
-    def to_file(self: "redis_db", filename: str):
+    def to_file(self: "redis_db", filename: str, compression: bool = True):
         """
         Dump the entire redis DB to a JSON file.
         """
@@ -301,9 +308,11 @@ class redis_db:
             )
 
         with open(filename, "w") as f:
-            f.write(self.to_json())
+            f.write(self.to_json(compression=compression))
 
-    def to_file_stream(self: "redis_db", filename: str):
+    def to_file_stream(
+        self: "redis_db", filename: str, compression: bool = True
+    ):
         """
         to_json returns a giant dict of the entire DB which can be serialised
         as a JSON string. The DB is now too big for this (the server runs out
@@ -315,16 +324,16 @@ class redis_db:
             )
 
         with open(filename, "w") as f:
-            for line in self.to_json_stream():
+            for line in self.to_json_stream(compression=compression):
                 f.write(line)
 
-    def to_json(self: "redis_db") -> str:
+    def to_json(self: "redis_db", compression: bool = True) -> str:
         """
         Dump the entire redis DB to JSON
         """
         d: dict = {}
         for k in self.r.keys("*"):
-            d[k.decode("utf-8")] = self.get(key=k)
+            d[k.decode("utf-8")] = self.get(key=k, compression=compression)
 
         if d:
             json_str = json.dumps(d)
@@ -333,12 +342,12 @@ class redis_db:
         else:
             raise ValueError("Database is empty")
 
-    def to_json_stream(self: "redis_db") -> Iterable:
+    def to_json_stream(self: "redis_db", compression: bool = True) -> Iterable:
         yield ("{")
         keys = self.r.keys("*")
         for idx, key in enumerate(keys):
             k = key.decode("utf-8")
-            d = json.dumps({k: self.get(key=k)})
+            d = json.dumps({k: self.get(key=k, compression=compression)})
             if idx == len(keys) - 1:
                 yield (f"{d[1:-1]}")
             else:
