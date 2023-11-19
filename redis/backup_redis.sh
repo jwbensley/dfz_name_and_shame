7740a2dd8744fa352fe294deec1a61ad8c35028c
James Bensley <jwbensley@gmail.com>
2023-11-19 11:31:57 +0100

    Run backup script in container

--- redis/backup_redis.sh
+++ redis/backup_redis.sh
@@ -5,14 +5,14 @@ set -eu
 # shellcheck disable=SC1091
 source /opt/dnas/venv/bin/activate
 
-datetime=$(date "+%Y-%m-%d")
+datetime=$(date "+%Y-%m-%d") # 2023-01-01
 backup_dir="/opt/dnas/redis/backups"
 backup_file="${backup_dir}/redis-${datetime}.json"
 
 mkdir -p "${backup_dir}"
 chown bensley:bensley "${backup_dir}"
-cd /opt/dnas/dnas/scripts/ || exit 1
-./redis_mgmt.py --stream --dump "${backup_file}"
+cd /opt/dnas/docker || exit 1
+docker-compose run --rm --name redis_backup dnas_parser -- /opt/dnas/dnas/scripts/redis_mgmt.py --stream --dump "${backup_file}"
 echo "Uncompressed size is: $(ls -lh "${backup_file}")"
 gzip "${backup_file}"
 echo "Compressed size is: $(ls -lh "${backup_file}.gz")"
