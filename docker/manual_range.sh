3df7e4249044c67fe278636e5c8b9752667762ca
James Bensley <1025875+jwbensley@users.noreply.github.com>
2023-11-19 12:38:50 +0100

    Fix arg pass-through

--- docker/manual_range.sh
+++ docker/manual_range.sh
@@ -16,13 +16,13 @@ set -e
 
 if [[ ${#} -lt 6 ]]
 then
-  echo "Wrong number of args ${#}."
+  echo "Wrong number of args: ${#}"
   echo "Call the script with at least 6 values, all space seperated."
   echo "Start year, start month, start day, end year, end month, end day."
   echo "These should have leading zeros e.g.,"
   echo "${0} 2023 09 01 2023 09 30"
   echo ""
-  echo "Any additional arguments are passed to the script."
+  echo "Any additional arguments are passed to the scripts."
   exit 1
 fi
 
@@ -53,7 +53,7 @@ do
       echo "doing ${year}${month}${day}:"
       docker-compose run --rm --name tmp_getter --entrypoint /opt/pypy dnas_getter -- \
       "${SCRIPTS_DIR}/get_mrts.py" \
-      --backfill --update --enabled --ymd "${year}${month}${day}" "${*}"
+      --backfill --update --enabled --ymd "${year}${month}${day}" "${@}"
     done
   done
 done
@@ -67,7 +67,7 @@ do
       echo "doing ${year}${month}${day}:"
       docker-compose run --rm --name tmp_parser --entrypoint /opt/pypy dnas_parser -- \
       "${SCRIPTS_DIR}/parse_mrts.py" \
-      --update --remove --enabled --no-multi --ymd "${year}${month}${day}" "${*}"
+      --update --remove --enabled --no-multi --ymd "${year}${month}${day}" "${@}"
     done
   done
 done
@@ -81,7 +81,7 @@ do
       echo "doing ${year}${month}${day}:"
       docker-compose run --rm --name tmp_stats --entrypoint /opt/pypy dnas_stats -- \
       "${SCRIPTS_DIR}/stats.py" \
-      --update --enabled --daily --ymd "${year}${month}${day}" "${*}"
+      --update --enabled --daily --ymd "${year}${month}${day}" "${@}"
     done
   done
 done
@@ -95,10 +95,10 @@ do
       echo "doing ${year}${month}${day}:"
       docker-compose run --rm --name tmp_git --entrypoint /opt/pypy dnas_stats -- \
       "${SCRIPTS_DIR}/git_reports.py" \
-      --generate --publish --ymd "${year}${month}${day}" "${*}"
+      --generate --publish --ymd "${year}${month}${day}" "${@}"
     done
   done
 done
 
 #docker-compose run --rm --name tmp_tweet dnas_stats -- \
-#"${SCRIPTS_DIR}/tweet.py" --generate --tweet --ymd "$1" "${*}"
+#"${SCRIPTS_DIR}/tweet.py" --generate --tweet --ymd "$1" "${@}"
