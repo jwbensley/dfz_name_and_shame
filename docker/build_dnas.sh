6d724c3034cf5c1d32f112fd514cde7adfd5a2e0
James Bensley <jwbensley@gmail.com>
2024-11-11 16:17:41 +0100

    Don't need venv for docker compose

--- docker/build_dnas.sh
+++ docker/build_dnas.sh
@@ -4,7 +4,6 @@ set -e
 
 DATA_DIR="/opt/dnas_data/"
 APP_DIR="/opt/dnas/"
-VENV_DIR="./venv/"
 
 mkdir -p "$DATA_DIR"
 mkdir -p "$APP_DIR"
@@ -17,14 +16,6 @@ else
     git checkout main
     git pull origin main --rebase
 fi
-
-if [ ! -d "$VENV_DIR" ]
-then
-    python3 -m virtualenv venv
-fi
-# Shellcheck can't follow "source" imports:
-# shellcheck disable=SC1091
-source venv/bin/activate
 
 cd docker/
 # Ensure latest images are present
