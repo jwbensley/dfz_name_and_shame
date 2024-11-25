76ceee942472dfe2e01c40e6d8ff3eab1d0bf71a
James Bensley <jwbensley@users.noreply.github.com>
2024-11-25 07:46:23 +0100

    Update test_clone.py

--- test_clone.py
+++ test_clone.py
@@ -1,19 +1,21 @@
 import subprocess
-subprocess.run(
-    ["mkdir", "-p", "/opt/dnas_data/"],
-    capture_output=True,
-)
-ret = subprocess.run(
-    ["git", "clone", "git@github.com:DFZ-Name-and-Shame/dnas_stats.git", "/opt/dnas_data/"],
-    capture_output=True,
-)
-print(ret.returncode)
-print(ret.stdout.decode())
-print(ret.stderr.decode())
-ret = subprocess.run(
-    ["ls", "-l", "/opt/dnas_data/"],
-    capture_output=True,
-)
-print(ret.returncode)
-print(ret.stdout.decode())
-print(ret.stderr.decode())
+
+def test_clone():
+    subprocess.run(
+        ["mkdir", "-p", "/opt/dnas_data/"],
+        capture_output=True,
+    )
+    ret = subprocess.run(
+        ["git", "clone", "git@github.com:DFZ-Name-and-Shame/dnas_stats.git", "/opt/dnas_data/"],
+        capture_output=True,
+    )
+    print(ret.returncode)
+    print(ret.stdout.decode())
+    print(ret.stderr.decode())
+    ret = subprocess.run(
+        ["ls", "-l", "/opt/dnas_data/"],
+        capture_output=True,
+    )
+    print(ret.returncode)
+    print(ret.stdout.decode())
+    print(ret.stderr.decode())
