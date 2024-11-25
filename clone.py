77d2c8ee6140e8fb45162ab535035c549d5feae8
James Bensley <jwbensley@users.noreply.github.com>
2024-11-25 07:41:07 +0100

    Update clone.py

--- clone.py
+++ clone.py
@@ -1,6 +1,17 @@
 import subprocess
+subprocess.run(
+    ["mkdir", "-p", "/opt/dnas_data/"],
+    capture_output=True,
+)
 ret = subprocess.run(
-    ["git", "clone", "git@github.com:DFZ-Name-and-Shame/dnas_stats.git"],
+    ["git", "clone", "git@github.com:DFZ-Name-and-Shame/dnas_stats.git", "/opt/dnas_data/"],
+    capture_output=True,
+)
+print(ret.returncode)
+print(ret.stdout.decode())
+print(ret.stderr.decode())
+ret = subprocess.run(
+    ["ls", "-l", "/opt/dnas_data/"],
     capture_output=True,
 )
 print(ret.returncode)
